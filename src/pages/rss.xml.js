acd14c25486a0105777c4ee81a635cc7e3bd9931
jiyuujin <y-kitamura@wevnal.co.jp>
2025-09-22 18:46:46 +0900

    use the astrojs/rss plugin instead
    - remove feeds script
    - add astrojs/rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,24 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
+/**
+ * Creates an RSS feed for the blog posts.
+ * https://docs.astro.build/ja/recipes/rss/
+ */
+export async function GET(context) {
+  return rss({
+    title: 'Soratabi Blog',
+    description: 'The perfect product for your aviation blog.',
+    site: context.site,
+    items: await Promise.all(
+      Object.entries(import.meta.glob('./blog/p/*.{md,mdx}')).map(async ([path, resolver]) => {
+        const post = await resolver()
+        return {
+          title: post.frontmatter.title,
+          pubDate: new Date(post.frontmatter.date),
+          description: '',
+          link: path.replace('./blog/p', '/blog/p').replace(/\.mdx?$/, ''),
+        }
+      }),
+    ),
+  })
+}
