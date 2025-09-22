e17e830285e84b89019b63a31c24100baf7a297d
jiyuujin <y-kitamura@wevnal.co.jp>
2025-09-22 16:27:32 +0900

    rss test

---
+++ src/pages/rss.xml.mjs
@@ -0,0 +1,14 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
+/**
+ * Creates an RSS feed for the blog posts.
+ * https://docs.astro.build/ja/recipes/rss/
+ */
+export async function GET(context) {
+  return rss({
+    title: 'Soratabi Blog',
+    description: 'The perfect product for your aviation blog.',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./blog/p/*.{md,mdx}')),
+  })
+}
